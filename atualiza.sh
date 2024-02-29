7337bb98bc398e5e1bf0e1fc080ff611245a66e5
erick <erick.ozaki@gmail.com>
2024-02-29 20:04:17 +0000

    script pra atualizar links simbolicos

---
+++ atualiza.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+rm so_2023_trab1_validator_v1/so_utils.sh
+rm so_2023_trab1_validator_v1/_etc_passwd
+
+cp /home/so/utils/bin/so_utils.sh so_2023_trab1_validator_v1/
+cp /home/so/trabalho-2023-2024/utils/parte-1/_etc_passwd so_2023_trab1_validator_v1/
