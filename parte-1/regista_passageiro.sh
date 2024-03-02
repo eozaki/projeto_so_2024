80625697479a96f7d81ae659274df403378f7e40
Erick Ozaki <erick@gaming-rig.Home>
2024-03-02 23:58:35 +0000

    Feito até 1.2.7

--- parte-1/regista_passageiro.sh
+++ parte-1/regista_passageiro.sh
@@ -25,8 +25,8 @@ so_success() {
 #####################
 # CONSTANTS
 #####################
-NUMBER_REGEX='^\d+$'
-VAT_LENGTH_REGEX='^\d{9}$'
+NUMBER_REGEX='^[0-9]+$'
+VAT_LENGTH_REGEX='^[0-9]{9}$'
 PASSENGERS_FILE=passageiros.txt
 TIGRE_USERS_FILE=/etc/passwd
 
@@ -41,91 +41,76 @@ fi
 
 ## S1.1.2. Valida se o argumento <Nome> corresponde ao nome de um utilizador do servidor Tigre. Se não corresponder ao nome de nenhum utilizador do Tigre, dá so_error S1.1.2 e termina. Senão, dá so_success S1.1.2.
 userOnPasswdFile=$(grep "$1" $TIGRE_USERS_FILE -c)
-if ! [[ $userOnPasswdFile -lt 1 ]]; then
+if [[ $userOnPasswdFile -lt 1 ]]; then
   so_error "S1.1.2"
   exit 1
-fi
-
-if [[ $(cat /etc/passwd | grep -c $1) -eq 1 ]]; then
-  so_success "S1.1.2"
 else
-  so_error "S1.1.2"
-  exit 1
+  so_success "S1.1.2"
 fi
 
 ## S1.1.3. Valida se o argumento <Saldo a adicionar> tem formato “number” (inteiro positivo ou 0). Se não tiver, dá so_error S1.1.3 e termina. Caso contrário, dá so_success S1.1.3.
-validateAmountIsNumberValue() {
-  if ! [[ $1 =~ $NUMBER_REGEX ]]; then
-    so_error "S1.1.3"
-    exit 1
-  fi
-
+if ! [[ $3 =~ $NUMBER_REGEX ]]; then
+  so_error "S1.1.3"
+  exit 1
+else
   so_success "S1.1.3"
-}
+fi
+
 
 ## S1.1.4. Valida se o argumento opcional <NIF> (só no caso de ser passado, i.e., se tiver valor) tem formato “number” com 9 (nove) dígitos. Se não for, dá so_error S1.1.4 e termina. Caso contrário, dá so_success S1.1.4.
-validateVATNumber() {
-  if ! [[ $1 =~ $NUMBER_REGEX && $1 =~ VAT_LENGTH_REGEX ]]; then
+if [[ -n "$4" ]]; then
+  if ! [[ $4 =~ $NUMBER_REGEX && $4 =~ $VAT_LENGTH_REGEX ]]; then
     so_error "S1.1.4"
     exit 1
   fi
+fi
 
-  so_success "S1.1.4"
-}
+so_success "S1.1.4"
 
 ## S1.2. Associa os dados passados com a base de dados dos passageiros registados:
 ## S1.2.1. Verifica se o ficheiro passageiros.txt existe. Se o ficheiro existir, dá so_success S1.2.1 e continua no passo S1.2.3. Se não existir, dá so_error S1.2.1, e continua.
-checkForPassengersDataFile() {
-  if ! [[ -f $PASSENGERS_FILE ]]; then
-    so_error "S1.2.1"
-  fi
-
+if [[ -f $PASSENGERS_FILE ]]; then
   so_success "S1.2.1"
-}
+  passengersFileExists=true
+else
+  so_error "S1.2.1"
+fi
 
 ## S1.2.2. Cria o ficheiro passageiros.txt. Se der erro, dá so_error S1.2.2 e termina. Senão, dá so_success S1.2.2.
-createPassengersDataFile() {
+if ! [[ $passengesFileExists ]]; then
   if ! [[ $(touch $PASSENGERS_FILE) -eq 0 ]]; then
     so_error "S1.2.2"
   fi
 
   so_success "S1.2.2"
-}
+fi
+
 
 ## S1.2.3. Caso o passageiro <Nome> passado já exista no ficheiro passageiros.txt, dá so_success S1.2.3, e continua no passo S1.3.  Senão, dá so_error S1.2.3, e continua.
-findPassengerOnList() {
-  countOfNameOccurences=$(grep $1 $PASSENGERS_FILE -c)
-  if [[ $countOfNameOccurences -gt 0 ]]; then
-    so_success "S1.2.3"
-  else
-    so_error "S1.2.3"
-  fi
-}
+if [[ $(grep "$1" $PASSENGERS_FILE -c) -gt 0 ]]; then
+  so_success "S1.2.3"
+else
+  so_error "S1.2.3"
 
-## S1.2.4. Como o passageiro <Nome> não existe no ficheiro, terá de o registar. Para isso, valida se <NIF> (campo opcional) foi mesmo passado. Se não foi, dá so_error S1.2.4 e termina. Senão, dá so_success S1.2.4.
-checkVAT() {
-  if ! [[ -n "$1" ]]; then
+  ## S1.2.4. Como o passageiro <Nome> não existe no ficheiro, terá de o registar. Para isso, valida se <NIF> (campo opcional) foi mesmo passado. Se não foi, dá so_error S1.2.4 e termina. Senão, dá so_success S1.2.4.
+  if ! [[ -n "$4" ]]; then
     so_error "S1.2.4"
     exit 1
   fi
 
   so_success "S1.2.4"
-}
 
-
-## S1.2.5. Define o campo <ID_passageiro>, como sendo o UserId Linux associado ao utilizador de nome <Nome> no servidor Tigre. Em caso de haver algum erro na operação, dá so_error S1.2.5 e termina. Caso contrário, dá so_success S1.2.5 <ID_passageiro> (substituindo pelo campo definido).
-getTigreIdForUser() {
+  ## S1.2.5. Define o campo <ID_passageiro>, como sendo o UserId Linux associado ao utilizador de nome <Nome> no servidor Tigre. Em caso de haver algum erro na operação, dá so_error S1.2.5 e termina. Caso contrário, dá so_success S1.2.5 <ID_passageiro> (substituindo pelo campo definido).
   if ! [[ -f $TIGRE_USERS_FILE ]]; then
     so_error "S1.2.5"
+  else
+    so_success "S1.2.5"
   fi
 
-  return $(grep $1 $TIGRE_USERS_FILE | cut -f3 -d ':')
-}
+  passengerId=$(grep "$1" $TIGRE_USERS_FILE | cut -f3 -d ':')
 
-## S1.2.6. Define o campo <Email>, gerado a partir do <Nome> introduzido pelo utilizador, usando apenas o primeiro e o último nome (dica: https://moodle23.iscte-iul.pt/mod/forum/discuss.php?d=5344), convertendo-os para minúsculas apenas, colocando um ponto entre os dois nomes, e domínio iscteflight.pt. Assim sendo, um exemplo seria “david.gabriel@iscteflight.pt”. Se houver algum erro na operação (e.g., o utilizador “root” tem menos de 2 nomes), dá so_error S1.2.6 e termina. Caso contrário, dá so_success S1.2.6 <Email> (substituindo pelo campo gerado). Ao registar um novo passageiro no sistema, o número inicial de <Saldo> tem o valor 0 (zero).
-createEmailAddressForUser() {
+  ## S1.2.6. Define o campo <Email>, gerado a partir do <Nome> introduzido pelo utilizador, usando apenas o primeiro e o último nome (dica: https://moodle23.iscte-iul.pt/mod/forum/discuss.php?d=5344), convertendo-os para minúsculas apenas, colocando um ponto entre os dois nomes, e domínio iscteflight.pt. Assim sendo, um exemplo seria “david.gabriel@iscteflight.pt”. Se houver algum erro na operação (e.g., o utilizador “root” tem menos de 2 nomes), dá so_error S1.2.6 e termina. Caso contrário, dá so_success S1.2.6 <Email> (substituindo pelo campo gerado). Ao registar um novo passageiro no sistema, o número inicial de <Saldo> tem o valor 0 (zero).
   fullName=($1)
-
   if [[ ${#fullName[@]} -lt 2 ]]; then
     so_error "S1.2.6"
     exit 1
@@ -134,10 +119,9 @@ createEmailAddressForUser() {
   email=${fullName[0]}.${fullName[${#fullName[@]} - 1]}@iscteflight.pt
   email=$(echo "$email" | awk '{print tolower($0)}')
 
-  return 0
-}
-
-## S1.2.7. Regista o utilizador numa nova linha no final do ficheiro passageiros.txt, seguindo a sintaxe:   <ID_passageiro>:<NIF>:<Nome>:<Email>:<Senha>:<Saldo>. Em caso de haver algum erro na operação (e.g., erro na escrita do ficheiro), dá so_error S1.2.7 e termina. Caso contrário, dá so_success S1.2.7 <linha> (substituindo pela linha completa escrita no ficheiro).
+  ## S1.2.7. Regista o utilizador numa nova linha no final do ficheiro passageiros.txt, seguindo a sintaxe:   <ID_passageiro>:<NIF>:<Nome>:<Email>:<Senha>:<Saldo>. Em caso de haver algum erro na operação (e.g., erro na escrita do ficheiro), dá so_error S1.2.7 e termina. Caso contrário, dá so_success S1.2.7 <linha> (substituindo pela linha completa escrita no ficheiro).
+  echo "$passengerId:$4:$1:$email:$2:0" >> $PASSENGERS_FILE
+fi
 
 ## S1.3. Adiciona créditos na conta de um passageiro que existe no ficheiro passageiros.txt:
 ## S1.3.1. Tendo já encontrado um “match” passageiro com o Nome <Nome> no ficheiro, valida se o campo <Senha> passado corresponde à senha registada no ficheiro. Se não corresponder, dá so_error S1.3.1 e termina. Caso contrário, dá so_success S1.3.1.
