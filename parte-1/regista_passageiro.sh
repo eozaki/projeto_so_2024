6a6c40f2921face472b9e0a45f884cb635826e5c
Erick Cordeiro Ozaki <a122088@iscte.pt>
2024-03-05 21:38:57 +0000

    works all the way to 1.2.7

--- parte-1/regista_passageiro.sh
+++ parte-1/regista_passageiro.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-# SO_HIDE_DEBUG=1                   ## Uncomment this line to hide all @DEBUG statements
+#! /bin/bash
 # SO_HIDE_COLOURS=1                 ## Uncomment this line to disable all escape colouring
-# . so_utils.sh                     ## This is required to activate the macros so_success, so_error, and so_debug
+. so_utils.sh                     ## This is required to activate the macros so_success, so_error, and so_debug
 
 ###############################################################################
 ## ISCTE-IUL: Trabalho prático de Sistemas Operativos 2023/2024, Enunciado Versão 3+
@@ -14,13 +13,13 @@
 ###############################################################################
 
 ## Este script é invocado quando um novo passageiro se regista na plataforma IscteFlight. Este script recebe todos os dados por argumento, na chamada da linha de comandos. Os passageiros são registados no ficheiro passageiros.txt. Deve receber as informações do passageiro como argumentos pela seguinte ordem: <Nome:string> <Senha:string>  <Saldo a adicionar:number> [<NIF:number>]
-so_error() {
-  echo "AAAAAAAAAAAAAAAAAAAAAA$1"
-}
-
-so_success() {
-  echo "daora!$1"
-}
+# so_error() {
+#   echo "AAAAAAAAAAAAAAAAAAAAAA$1"
+# }
+# 
+# so_success() {
+#   echo "daora!$1"
+# }
 
 #####################
 # CONSTANTS
@@ -40,8 +39,9 @@ else
 fi
 
 ## S1.1.2. Valida se o argumento <Nome> corresponde ao nome de um utilizador do servidor Tigre. Se não corresponder ao nome de nenhum utilizador do Tigre, dá so_error S1.1.2 e termina. Senão, dá so_success S1.1.2.
-userOnPasswdFile=$(grep "$1" $TIGRE_USERS_FILE -c)
-if [[ $userOnPasswdFile -lt 1 ]]; then
+fullName=$1
+userOnPasswdFile=$(cat $TIGRE_USERS_FILE|grep -E ":$fullName,")
+if [[ -z "$userOnPasswdFile" ]]; then
   so_error "S1.1.2"
   exit 1
 else
@@ -62,10 +62,10 @@ if [[ -n "$4" ]]; then
   if ! [[ $4 =~ $NUMBER_REGEX && $4 =~ $VAT_LENGTH_REGEX ]]; then
     so_error "S1.1.4"
     exit 1
+  else
+    so_success "S1.1.4"
   fi
 fi
-
-so_success "S1.1.4"
 
 ## S1.2. Associa os dados passados com a base de dados dos passageiros registados:
 ## S1.2.1. Verifica se o ficheiro passageiros.txt existe. Se o ficheiro existir, dá so_success S1.2.1 e continua no passo S1.2.3. Se não existir, dá so_error S1.2.1, e continua.
@@ -101,26 +101,29 @@ else
   so_success "S1.2.4"
 
   ## S1.2.5. Define o campo <ID_passageiro>, como sendo o UserId Linux associado ao utilizador de nome <Nome> no servidor Tigre. Em caso de haver algum erro na operação, dá so_error S1.2.5 e termina. Caso contrário, dá so_success S1.2.5 <ID_passageiro> (substituindo pelo campo definido).
+  fullName=$1
   if ! [[ -f $TIGRE_USERS_FILE ]]; then
     so_error "S1.2.5"
   else
-    so_success "S1.2.5"
+    passengerId=$(grep "$fullName" $TIGRE_USERS_FILE | cut -f1 -d ':')
+    so_success "S1.2.5" $passengerId
   fi
-
-  passengerId=$(grep "$1" $TIGRE_USERS_FILE | cut -f3 -d ':')
 
   ## S1.2.6. Define o campo <Email>, gerado a partir do <Nome> introduzido pelo utilizador, usando apenas o primeiro e o último nome (dica: https://moodle23.iscte-iul.pt/mod/forum/discuss.php?d=5344), convertendo-os para minúsculas apenas, colocando um ponto entre os dois nomes, e domínio iscteflight.pt. Assim sendo, um exemplo seria “david.gabriel@iscteflight.pt”. Se houver algum erro na operação (e.g., o utilizador “root” tem menos de 2 nomes), dá so_error S1.2.6 e termina. Caso contrário, dá so_success S1.2.6 <Email> (substituindo pelo campo gerado). Ao registar um novo passageiro no sistema, o número inicial de <Saldo> tem o valor 0 (zero).
-  fullName=($1)
-  if [[ ${#fullName[@]} -lt 2 ]]; then
+  if ! [[ $fullName =~ [A-Z][a-z]+\ [A-Z][a-z]+ ]]; then
     so_error "S1.2.6"
     exit 1
+  else
+    firstName=$(echo $fullName | awk '{print tolower($1)}')
+    lastName=$(echo $fullName | awk '{print tolower($(NF))}')
+    email="$firstName.$lastName"@iscteflight.pt
+    so_success "S1.2.6" $email
   fi
 
-  email=${fullName[0]}.${fullName[${#fullName[@]} - 1]}@iscteflight.pt
-  email=$(echo "$email" | awk '{print tolower($0)}')
-
   ## S1.2.7. Regista o utilizador numa nova linha no final do ficheiro passageiros.txt, seguindo a sintaxe:   <ID_passageiro>:<NIF>:<Nome>:<Email>:<Senha>:<Saldo>. Em caso de haver algum erro na operação (e.g., erro na escrita do ficheiro), dá so_error S1.2.7 e termina. Caso contrário, dá so_success S1.2.7 <linha> (substituindo pela linha completa escrita no ficheiro).
-  echo "$passengerId:$4:$1:$email:$2:0" >> $PASSENGERS_FILE
+  if ! [[ $(echo "$passengerId:$4:$1:$email:$2:$3" >> $PASSENGERS_FILE) ]]; then
+    so_success "S1.2.7"
+  fi
 fi
 
 ## S1.3. Adiciona créditos na conta de um passageiro que existe no ficheiro passageiros.txt:
