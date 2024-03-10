92765f49fee2c0174e3e7e7fdd28edc76ba3bf1d
Erick Cordeiro Ozaki <a122088@iscte.pt>
2024-03-10 00:22:33 +0000

    wip 4.2

--- parte-1/estado_voos.sh
+++ parte-1/estado_voos.sh
@@ -6,7 +6,7 @@
 ###############################################################################
 ## ISCTE-IUL: Trabalho prático de Sistemas Operativos 2023/2024, Enunciado Versão 3+
 ##
-## Aluno: Nº:       Nome:
+## Aluno: Nº: 122088       Nome: Erick Cordeiro Ozaki
 ## Nome do Módulo: S3. Script: estado_voos.sh
 ## Descrição/Explicação do Módulo:
 ##
@@ -15,13 +15,64 @@
 
 ## Este script não recebe nenhum argumento, e é responsável pelo relatório do estado dos voos que pertencem à plataforma IscteFlight.
 
+FLIGHT_FILE="voos.txt"
+FLIGHT_PAGE="voos_disponiveis.html"
+
 ## S3.1. Validações:
 ## S3.1.1. O script valida se o ficheiro voos.txt existe. Se não existir, dá so_error e termina. Senão, dá so_success.
+if [[ -f "$FLIGHT_FILE" ]]; then
+  so_success "S3.1.1"
+else
+  so_error "S3.1.1"
+  exit 1
+fi
 
 ## S3.1.2. O script valida se os formatos de todos os campos de cada linha do ficheiro voos.txt correspondem à especificação indicada em S2, nomeadamente se respeitam os formatos de data e de hora. Se alguma linha não respeitar, dá so_error <conteúdo da linha> e termina. Caso contrário, dá so_success.
+# FR2101:Lisboa:Dubai:2024-05-23:08h00:320:210:35
+FLIGHT_REGEX='^.{6}:([A-Z]|[a-z]|\ )+:([A-Z]|[a-z]|\ )+:[0-9]{4}-[0-9]{2}-[0-9]{2}:[0-9]{2}h[0-9]{2}:[0-9]+:[0-9]+:[0-9]+'
+while IFS="" read -r linha
+do
+  if ! [[ "$linha" =~ $FLIGHT_REGEX ]]; then
+    so_error 'S3.1.2' "$linha"
+    exit 1
+  fi
+done < "$FLIGHT_FILE"
+
+so_success 'S3.1.2'
 
 ## S3.2. Processamento do script:
 ## S3.2.1. O script cria uma página em formato HTML, chamada voos_disponiveis.html, onde lista os voos com lugares disponíveis, indicando nº, origem, destino, data, hora, lotação, nº de lugares disponíveis, e nº de lugares ocupados (para isso deve calcular a diferença dos anteriores). Em caso de erro (por exemplo, se não conseguir escrever no ficheiro), dá so_error e termina. Caso contrário, dá so_success.
+# <h2>Voo: PT6202, De: Tóquio Para: Singapura, Partida em 2024-03-10 16h45</h2>
+# <ul>
+# <li><b>Lotação:</b> 210 Lugares</li>
+# <li><b>Lugares Disponíveis:</b> 35 Lugares</li>
+# <li><b>Lugares Ocupados:</b> 175 Lugares</li>
+# </ul>
+
+if [[ -f "$FLIGHT_PAGE" ]]; then
+  rm "$FLIGHT_PAGE"
+fi
+
+htmlContent="<html><head><meta charset=\"UTF-8\"><title>IscteFlight: Lista de Voos Disponíveis</title></head>\n"
+htmlContent=$htmlContent"<body><h1>Lista atualizada em $(date +'%Y-%m-%d %H:%M:%S')</h1>\n"
+while IFS=":" read -r codVoo origem destino data hora preco lotacao disponiveis
+do
+  if [[ "$disponiveis" -gt 0 ]]; then
+    htmlContent=$htmlContent"<h2>Voo: $codVoo, De: $origem Para: $destino, Partida em $data $hora</h2>\n"
+    htmlContent=$htmlContent"<ul>\n"
+    htmlContent=$htmlContent"<li><b>Lotação:</b> $lotacao Lugares</li>\n"
+    htmlContent=$htmlContent"<li><b>Lugares Disponíveis:</b> $disponiveis Lugares</li>\n"
+    htmlContent=$htmlContent"<li><b>Lugares Ocupados:</b> $(($lotacao - $disponiveis)) Lugares</li>\n"
+    htmlContent=$htmlContent"</ul>\n"
+  fi
+done < "$FLIGHT_FILE"
+htmlContent=$htmlContent"</body></html>"
+
+if [[ $(echo -e "$htmlContent" > "$FLIGHT_PAGE") -eq 0 ]]; then
+  so_success 'S3.2.1'
+else
+  so_error 'S3.2.1'
+fi
 
 ## S3.3. Invocação do script estado_voos.sh:
 ## S3.3.1. Altere o ficheiro cron.def fornecido, por forma a configurar o seu sistema para que o script seja executado de hora em hora, diariamente. Nos comentários no início do ficheiro cron.def, explique a configuração realizada, e indique qual o comando que deveria utilizar para despoletar essa configuração. O ficheiro cron.def alterado deverá ser submetido para avaliação juntamente com os outros Shell scripts.
--- parte-1/stats.sh
+++ parte-1/stats.sh
@@ -6,7 +6,7 @@
 ###############################################################################
 ## ISCTE-IUL: Trabalho prático de Sistemas Operativos 2023/2024, Enunciado Versão 3+
 ##
-## Aluno: Nº:       Nome:
+## Aluno: Nº: a122088      Nome: Erick Cordeiro Ozaki
 ## Nome do Módulo: S4. Script: stats.sh
 ## Descrição/Explicação do Módulo:
 ##
@@ -15,10 +15,47 @@
 
 ## Este script obtém informações sobre o sistema, afixando resultados diferentes no STDOUT consoante os argumentos passados na sua invocação. A sintaxe resumida é: ./stats.sh <passageiros>|<top <nr>>
 
+REPORT_FILE='relatorio_reservas.txt'
+
 ## S4.1. Validações:
 ## S4.1.1. Valida os argumentos recebidos e, conforme os mesmos, o número e tipo de argumentos recebidos. Se não respeitarem a especificação, dá so_error e termina. Caso contrário, dá so_success.
+if [[ $# -eq 0 ]]; then
+  so_error 'S4.1.1'
+  exit 1
+fi
+
+if [[ ! $1 == 'passageiros' ]] && [[ ! $1 == 'top' ]]; then
+  so_error 'S4.1.1'
+fi
+
+if [[ $1 == 'top' ]] && [[ $# -lt 2 ]]; then
+  so_error 'S4.1.1'
+fi
+
+so_success 'S4.1.1'
 
 ## S4.2. Invocação do script:
 ## S4.2.1. Se receber o argumento passageiros, (i.e., ./stats.sh passageiros) cria um ficheiro stats.txt onde lista o nome de todos os utilizadores que fizeram reservas, por ordem decrescente de número de reservas efetuadas, e mostrando o seu valor total de compras. Em caso de erro (por exemplo, se não conseguir ler algum ficheiro necessário), dá so_error e termina. Caso contrário, dá so_success e cria o ficheiro. Em caso de empate no número de reservas, lista o primeiro do ficheiro. Preste atenção ao tratamento do singular e plural quando se escreve “reserva” no ficheiro).
+
+if [[ "$1" == "passageiros" ]]; then
+  TEMP_STATS='stats.tmp'
+  if [[ -f "$TEMP_STATS" ]]; then
+    rm -f "$TEMP_STATS"
+  fi
+
+  users=$(cat "$REPORT_FILE" | awk -F ':' '{print $6}' | uniq)
+
+  tmp=""
+  while IFS=":" read -r idR codigo origem destino preco user timestamp
+  soma=0
+  do
+    if [[ $user == $usuario ]]; then
+    fi
+  done < $users
+
+  if [[ ! $(echo -e "$tmp" > "$TEMP_STATS") -eq 0 ]]; then
+    so_error 'S4.2.1'
+  fi
+fi
 
 ## S4.2.2. Se receber o argumento top <nr:number>, (e.g., ./stats.sh top 4), cria um ficheiro stats.txt onde lista os <nr> (no exemplo, os 4) voos mais rentáveis (que tiveram melhores receitas de vendas), por ordem decrescente. Em caso de erro (por exemplo, se não conseguir ler algum ficheiro necessário), dá so_error e termina. Caso contrário, dá so_success e cria o ficheiro. Em caso de empate, lista o primeiro do ficheiro.
